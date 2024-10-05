c7da9e60ff988fae5e028f0af25ccced91e330f1
milind bhatt <onlyforwork2609@gmail.com>
2024-10-05 15:36:19 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "milindbhatt26@gmail.com"
+var accountpassword = "12345"
+accountcity = "roorkee"
+let accountstate
+
+accountEmail = "hc@hc.com"
+
+accountpassword = "67676767"
+accountcity = "dehradun"
+console.log(accountId);
+
+/* refer to not use var cuz its not scope defined */
+console.table([accountId, accountEmail, accountpassword, accountcity]);
+console.log(accountstate);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code as newer version
+
+//alert(3 + 3)// we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) //code readability should be high 
+
+console.log("hitesh")
+let name = "milind"
+let age = 18 
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol=> unique
+
+
+// object 
+console.log()
+console.log(typeof null)    
